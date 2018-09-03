0b85f78de54302e0e5d4cff028234b0339f72872
Micah Wood <micah@wpscholar.com>
2018-09-03 17:11:54 -0400

    Added checks to ensure things don't break if a site's PHP or WP version isn't compatible.

--- shortcode-scrubber.php
+++ shortcode-scrubber.php
@@ -14,4 +14,14 @@
 define( 'SHORTCODE_SCRUBBER_FILE', __FILE__ );
 define( 'SHORTCODE_SCRUBBER_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
-require __DIR__ . '/includes/bootstrap.php';
+// Check plugin requirements
+global $pagenow;
+if ( 'plugins.php' === $pagenow ) {
+	require( dirname( __FILE__ ) . '/includes/plugin-check.php' );
+	$plugin_check = new Shortcode_Scrubber_Plugin_Check( __FILE__ );
+	$plugin_check->min_php_version = '5.6';
+	$plugin_check->min_wp_version = '3.2';
+	$plugin_check->check_plugin_requirements();
+}
+
+require dirname( __FILE__ ) . '/includes/bootstrap.php';
