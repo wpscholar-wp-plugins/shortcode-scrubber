818d1193dcf21066bf7c6fdcca645dadaee1069f
Micah Wood <micah@wpscholar.com>
2018-09-03 17:12:56 -0400

    Added description to manage shortcode filters page.

--- templates/shortcode-manage-filters.php
+++ templates/shortcode-manage-filters.php
@@ -10,6 +10,7 @@ if ( empty( $current_shortcode ) ) {
 ?>
 <div class="wrap">
     <h1 class="wp-heading-inline"><?php esc_html_e( 'Manage Shortcode Filters', 'shortcode-scrubber' ); ?></h1>
+    <p><?php esc_html_e( 'Shortcode filters allow you control how specific shortcodes are rendered on your site. By default, they are temporary and easily reversible.', 'shortcode-scrubber' ); ?></p>
 	<?php
 	$template = $current_shortcode ? 'form-manage-shortcode.php' : 'form-select-shortcode.php';
 	require SHORTCODE_SCRUBBER_DIR . '/templates/' . $template;
